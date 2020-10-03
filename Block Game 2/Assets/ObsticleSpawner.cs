ce8b62f7d4c180062d669ed13281d3e14486f2d2
Karl Thaler <karlthaler5@gmail.com>
2020-10-02 18:26:53 -0700

    A simple block game
    a simple block game followed from tutorial by Sebastian Lague

---
+++ Block Game 2/Assets/ObsticleSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticleSpawner : MonoBehaviour
+{
+
+    Vector2 screenHalfSize;
+
+    public GameObject obsticle;
+
+    float nextSpawnTime;
+    public Vector2 secondsBetweenSpawnTimeMinMax;
+
+    public Vector2 spawnSizeMinMax;
+    public float spawnAngleMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenHalfSize = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Time.time > nextSpawnTime)
+        {
+            float secondsBetweenSpawnTime = Mathf.Lerp(secondsBetweenSpawnTimeMinMax.y, secondsBetweenSpawnTimeMinMax.x, Difficulty.GetDifficultyPercent());
+
+            Vector2 position = new Vector2(Random.Range(-screenHalfSize.x, screenHalfSize.x), screenHalfSize.y + obsticle.transform.localScale.y);
+            float scaleSize = Random.Range(spawnSizeMinMax.x, spawnSizeMinMax.y);
+            float scaleAngle = Random.Range(spawnAngleMax, -spawnAngleMax);
+
+            nextSpawnTime = Time.time + secondsBetweenSpawnTime;
+            GameObject newObsticle = (GameObject)Instantiate(obsticle, position, Quaternion.Euler(Vector3.forward * scaleAngle));
+
+            newObsticle.transform.localScale = Vector2.one * scaleSize;
+        }
+        
+    }
+}
---
+++ Block Game 2/Assets/PlayerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 7f;
+
+    float halfScreenHorizontal;
+    float playerHalfWidth;
+
+    public event System.Action OnPlayerDeath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHalfWidth = transform.localScale.x / 2;
+        halfScreenHorizontal = Camera.main.aspect * Camera.main.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        Vector2 velocity = input * speed;
+
+        transform.Translate(velocity * Time.deltaTime);
+
+        if(transform.position.x < -halfScreenHorizontal - playerHalfWidth)
+        {
+            transform.position = new Vector2(halfScreenHorizontal, transform.position.y);
+        }
+        if (transform.position.x > halfScreenHorizontal + playerHalfWidth)
+        {
+            transform.position = new Vector2(-halfScreenHorizontal, transform.position.y);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Falling Block")
+        {
+            if(OnPlayerDeath != null)
+            {
+                OnPlayerDeath();
+            }
+            Destroy(gameObject);
+        }
+    }
+}
